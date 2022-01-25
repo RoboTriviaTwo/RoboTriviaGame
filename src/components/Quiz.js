9513af6975a40c9fcbfb903f5b31f3c9269312d7
Chris <christopher.yun.3@gmail.com>
2022-01-24 23:32:57 -0500

    Created useState to track current score

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -4,19 +4,21 @@ import { useState } from "react";
 const Quiz = (props) => {
 
   const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [currentScore, setCurrentScore] = useState(0);
+  // let currentPlayerScore = 0; 
 
   const handleAnswerClick = (event) => {
     const userAnswer = event.target.value;
     if (userAnswer === props.quizQuestions[currentQuestion].correct_answer){
       console.log("Correct!")
-    }else{
+      setCurrentScore(currentScore + 1);
+      // currentPlayerScore = currentPlayerScore + 1;
+    } else {
       console.log("Nope!")
     }
     if (currentQuestion < props.quizQuestions.length - 1 ){
       setCurrentQuestion(currentQuestion + 1)
-      console.log(currentQuestion);
-    } 
-    else {
+    } else {
       alert("end of quiz");
     }
     // console.log(currentQuestion);
@@ -40,6 +42,7 @@ const Quiz = (props) => {
                 )
               })
             }
+            <p>{currentScore}</p>
            
             
           </>
