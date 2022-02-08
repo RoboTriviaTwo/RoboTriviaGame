aecb4dfc2722c0e07c370028ad5ccc9535ebac00
Ashley <ashleythadickal@gmail.com>
2022-02-08 12:08:49 -0500

    correct error of != vs !==

--- src/components/UserSelectionForm.js
+++ src/components/UserSelectionForm.js
@@ -110,7 +110,7 @@ const UserSelectionForm = () => {
   const shuffleArr = (array) => {
     let currentIndex = array.length, randomIndex;
     // While there remain elements to shuffle...
-    while (currentIndex != 0) {
+    while (currentIndex !== 0) {
     // Pick a remaining element...
       randomIndex = Math.floor(Math.random() * currentIndex);
       currentIndex--;
