69df35e5eda18a38b44cd8d86894834b16eff687
Linda <lindashen3@gmail.com>
2022-02-14 22:06:50 -0500

    create assets folder to hold landing app img

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -4,7 +4,7 @@ const LandingPage = (() => {
     return (
         <>
             <div className="landingPage">
-                {/* container for the image such that it will stay on the landing page and not within other components */}
+            {/* container for the image such that it will stay on the landing page and not within other components */}
             </div>
             {/* link to go to the quiz */}
             <div className='startingLink'>
