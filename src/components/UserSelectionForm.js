909142cb5399f1ba12f13a97fa16238b657266ae
Linda <lindashen3@gmail.com>
2022-02-19 11:53:01 -0500

    add redirect file and removed unused component

--- src/components/UserSelectionForm.js
+++ src/components/UserSelectionForm.js
@@ -2,7 +2,6 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 import PlayerNames from './PlayerNames.js';
-import Quiz from './Quiz.js';
 
 const UserSelectionForm = (props) => {
 
